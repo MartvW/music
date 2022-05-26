905a9dd6269f8ef5be0dcad712d01571a99fab2e
Mart van Weeghel <martvanweeghel@icloud.com>
2022-05-27 00:31:08 +0200

    Refactor

--- src/ts/components/pages/homePage/HomePage.tsx
+++ src/ts/components/pages/homePage/HomePage.tsx
@@ -1,8 +1,8 @@
 const HomePage = () => {
     return (
         <div className='w-screen h-screen flex flex-col gap-5 items-center justify-center'>
-            <h1 className='text-5xl'>Music by Mart</h1>
-            <h1 className='text-5xl font-thin'>Under Construction</h1>
+            <h1 className='text-5xl'>Music by <span className='font-bold text-5xl'>Mart</span></h1>
+            <h3 className='text-3xl font-thin'>Under Construction</h3>
         </div>
     );
 };
