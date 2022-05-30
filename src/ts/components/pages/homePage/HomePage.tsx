c1f03256841faa3c74d5730053650b6240a4ab67
Mart van Weeghel <martvanweeghel@icloud.com>
2022-05-30 14:20:17 +0200

    Link to www.martvanweeghel.nl

--- src/ts/components/pages/homePage/HomePage.tsx
+++ src/ts/components/pages/homePage/HomePage.tsx
@@ -6,6 +6,7 @@ const HomePage = () => (
         <div className='md:whitespace-normal whitespace-nowrap'>
             <h1 className='md:text-5xl text-2xl'>Hi, I'm Mart.</h1>
             <p className='md:text-4xl text-xl'>A musician.</p>
+            <a href='https://martvanweeghel.nl' className='cursor-pointer ease-in-out duration-300 hover:border-b-2 hover:border-blue-500 md:text-2xl text-md text-gray-400 font-thin'>A software developer.</a>
         </div>
     </div>
 );
